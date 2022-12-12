484b004c5d64b4ee520efa46ca724546376c6d32
malikbensalem <60427086+malikbensalem@users.noreply.github.com>
2022-12-12 16:24:10 +0000

    Update turo.py

--- turo.py
+++ turo.py
@@ -1,3 +1,4 @@
+from email import header
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -5,31 +6,40 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import csv
-import re
-
 import pickle
 import os.path
+from datetime import datetime
 
-
+from dateutil.parser import parse
 
 driver = None
-turoCsv=[['Guest','Reg','Status','Start','End','Miles','Extras','Reimbursements','Trip Cost','Earnings']]
+counterCsv=0
+turoCsv=[]
 
-def getFloat(str) :  
-    str=  re.sub('[^0-9.]+', '', str).replace(",", ".")
-    return float(str)
+def getFloat(str) :    
+    result = ''
+    nums='1234567890.-'
+    for char in str:
+        if char in nums:
+            result += char
+            if char=='.':
+                nums=nums.replace('.','')
+            elif char=='-':
+                nums=nums.replace('-','')
+    if result=='':
+        result=0
+    return float(result) 
 
 def setUpDriver():
     options = webdriver.ChromeOptions() 
-    options.add_argument("headless")
-    options.add_argument("--headless")
-    options.add_argument('--disable-gpu')
+    
+    options.add_argument('--user-data-dir=C:/Users/Malik/AppData/Local/Google/Chrome/User Data/Default')
 
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
     options.add_argument('--disable-blink-features=AutomationControlled')
 
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options,executable_path="C:/Users/Malik/Desktop/projects/Turo-web-crawler/chromedriver.exe")
     driver.maximize_window()
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
@@ -50,6 +60,7 @@ def scrollDown():
 
      
 def login():
+    print('\n\n\n\n\n\n\n\n\n')
     if os.path.isfile('cookies.pkl'):
         cookies = pickle.load(open("cookies.pkl", "rb"))
         for cookie in cookies:
@@ -63,17 +74,18 @@ def login():
     pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
     
 def getTripUpcomingAll():
+    print('\n\n\n\n\n\n\n\n\n')
     print('Getting upcoming trips.')
     goToTrips('booked')
     getEles('div[data-testid="test-button-dropdown"]')[0].click()
 
-    carAmount=len(getEles('span.dropdownVehicleItem-license'))
-    
+    carAmount=len(getEles('div.css-tlftjr'))-2
+
     getEles('div[data-testid="test-button-dropdown"]')[0].click()
     for carIndex in range(carAmount):
         getEles('div[data-testid="test-button-dropdown"]')[0].click()
-        reg=getEles('span.dropdownVehicleItem-license')[carIndex].text
-        getEles('span.dropdownVehicleItem-license')[carIndex].click() 
+        reg=getEles('div.css-tlftjr')[carIndex].text
+        getEles('div.css-tlftjr')[carIndex].click() 
         time.sleep(2)
         tripAmount=len(getEles('a.css-1aq0sp2',2))
         print('\nGetting ',reg,' (',tripAmount,') -> ')
@@ -83,15 +95,16 @@ def getTripUpcomingAll():
                 getTripUpcomingSingle(getEles('a.css-1aq0sp2')[tripIndex].get_attribute('href'),reg)
 
 def getTripHistoryAll():
+    print('\n\n')
     print('Getting historic trips.')
     goToTrips('history')
     getEles('div[data-testid="test-button-dropdown"]')[0].click()
-    carAmount=len(getEles('span.dropdownVehicleItem-license'))
+    carAmount=len(getEles('div.css-tlftjr'))-2
     getEles('div[data-testid="test-button-dropdown"]')[0].click()
-    for carIndex in range(carAmount):
+    for carIndex in range(2,carAmount):
         getEles('div[data-testid="test-button-dropdown"]')[0].click()
-        reg=getEles('span.dropdownVehicleItem-license')[carIndex].text
-        getEles('span.dropdownVehicleItem-license')[carIndex].click()
+        reg=getEles('div.css-tlftjr')[carIndex].text
+        getEles('div.css-tlftjr')[carIndex].click()
         scrollDown()
         tripAmount=len(getEles('a.historyFeedItem-details'))
         print('\nGetting ',reg,' (',tripAmount,') -> ')
@@ -103,11 +116,13 @@ def goToTrips(status):
     driver.get('https://turo.com/gb/en/trips/'+status)
 
 def getTripHistorySingle(carLink,reg):
+    
     driver.execute_script("window.open('"+carLink+"','_blank');")
     driver.switch_to.window(driver.window_handles[1])
     if getEles('a.u-marginTopSmaller.u-displayBlock'):
+        miles=getEles('span.css-mrzgsa-StyledText')[0].text
         getEles('a.u-marginTopSmaller.u-displayBlock')[0].click()
-        getReceipt(reg,'History')
+        getReceipt(reg,'History',miles)
     else: 
         getReceiptCancelled(reg)
 
@@ -119,87 +134,96 @@ def getTripUpcomingSingle(carLink,reg):
     driver.switch_to.window(driver.window_handles[1])
 
     getEles('a.u-marginTopSmaller.u-displayBlock')[0].click()
-    getReceipt(reg,'booked')
+    getReceipt(reg,'booked','0')
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
     return
 
 def getReceiptCancelled(reg):
     carInfo={}
-    carInfo['reg']=reg
-    carInfo['status']='Cancelled'
+    carInfo['Registration']=reg
+    carInfo['Status']='Cancelled'
     year=''
 
-    carInfo['guest']=getEles('p.css-g36krj-StyledText')[0].text
-    carInfo['miles']='0'
-    carInfo['earnings']='0'
-    carInfo['tripPrice']='0'
-    carInfo['reimbursements']='0'
+    carInfo['Guest']=''
+    try:    
+        carInfo['Guest']=getEles('p.css-g36krj-StyledText')[0].text
+    except IndexError:
+        driver.execute_script("window.location.reload();") 
+        carInfo['Guest']=getEles('p.css-g36krj-StyledText',60)[0].text
+
 
     if getEles('h2.css-lb4jzt-StyledText',.001):
         year='-'+getEles('h2.css-lb4jzt-StyledText')[0].text.split(' ')[-1]
     else:
         return
 
-    carInfo['start']=getEles('#pageContainer-content > div.app > div.app-content > div > div > div > div.css-5uizmy-ReservationDetailsDesktopLayout-ReservationDetailsDesktopLayoutContainer.e6je8v60 > div.gutter--6.row.u-justifyContentBetween.u-marginTop2 > div.col.col--md7.col--lg8 > div > section:nth-child(1) > div.detailsSection-description.col.col--12 > div > div:nth-child(1) > p.css-9ld7or-StyledText-StyledScheduleDateTime')[0].text.split(',')[1].split(' ')
-    carInfo['start']=carInfo['start'][1]+'-'+carInfo['start'][0]+year
+    carInfo['Date Start']=parse(getEles('p.css-9ld7or-StyledText-StyledScheduleDateTime')[0].text+' '+getEles('p.css-mzrfmu-StyledText-StyledScheduleDateTime')[0].text+' '+year).strftime("%m/%d/%Y, %H:%M:%S")
 
-    carInfo['end']=getEles('#pageContainer-content > div.app > div.app-content > div > div > div > div.css-5uizmy-ReservationDetailsDesktopLayout-ReservationDetailsDesktopLayoutContainer.e6je8v60 > div.gutter--6.row.u-justifyContentBetween.u-marginTop2 > div.col.col--md7.col--lg8 > div > section:nth-child(1) > div.detailsSection-description.col.col--12 > div > div:nth-child(3) > p.css-9ld7or-StyledText-StyledScheduleDateTime')[0].text.split(',')[1].split(' ')
-    carInfo['end']=carInfo['end'][1]+'-'+carInfo['end'][0]+year
+    carInfo['Date End']=parse(getEles('p.css-9ld7or-StyledText-StyledScheduleDateTime')[1].text+' '+getEles('p.css-kg2jz6-StyledText-StyledScheduleDateTime')[0].text).strftime("%m/%d/%Y, %H:%M:%S")
 
     turoCsv.append(carInfo)
 
-def getReceipt(reg,status):
+def getReceipt(reg,status,miles):
     carInfo={}
-    carInfo['reg']=reg
-    carInfo['status']=status
+    carInfo['Registration']=reg
+    carInfo['Status']=status
+    carInfo['Miles']=getFloat(miles)
 
-    carInfo['start']=getEles('div.receiptSchedule p')[0].text.split(',')[1:]
-    carInfo['start']='-'.join(carInfo['start']).replace(' ','-')
-    carInfo['end']=getEles('div.receiptSchedule p')[2].text.split(',')[1:]
-    carInfo['end']='-'.join(carInfo['end']).replace(' ','-')
-    carInfo['guest']=getEles('#receipt-container > div > div:nth-child(6) > li > span.value > a')[0].text
-    carInfo['miles']='0'
+    carInfo['Date Start']=parse(getEles('span.e1glo9kk7',60)[0].text.replace('BST',''))
+    carInfo['Date End']=parse(getEles('span.e1glo9kk7')[1].text.replace('BST',''))
+    carInfo['Trip Lenght (Days)']=carInfo['Date Start']-carInfo['Date End']
+    carInfo['Date Start']=carInfo['Date Start'].strftime("%m/%d/%Y, %H:%M:%S")
+    carInfo['Date End']=carInfo['Date End'].strftime("%m/%d/%Y, %H:%M:%S")
 
-    if getEles('#receipt-container > div > div:nth-child(8) > li:nth-child(2) > span.value',0.01):
-        carInfo['miles']=getEles('#receipt-container > div > div:nth-child(8) > li:nth-child(2) > span.value')[0].text
-    
-    carInfo['reimbursements']='0'
-    if getEles('#receipt-container > div > div.reimbursements > ul > li:nth-last-child(1) > span.value.total > span',0.01):
-        carInfo['reimbursements']=getEles('#receipt-container > div > div.reimbursements > ul > li:nth-last-child(1) > span.value.total > span')[0].text
+    carInfo['Guest']=getEles('span.css-1kv227m-StyledText a')[0].text
 
-    carInfo['tripPrice']=getEles('#receipt-container > div > div.cost-details > ul > li:nth-last-child(1) > span.value.total > span')[0].text
-   
-    try:
-        carInfo['earnings']=getEles('#receipt-container > div > div.payment-details > ul > li:nth-child(6) > span.value.earned.total > span',.01)[0].text
-    except IndexError:
-        try:
-            carInfo['earnings']=getEles('#receipt-container > div > div.payment-details > ul > li:nth-child(3) > span.value.earned.total > span',.01)[0].text
-        except IndexError:
-            carInfo['earnings']='missing'
+    data=getEles('div.e1glo9kk6 div span')
+    for x in range(0,len(data),2):
+        carInfo[data[x].text]=data[x+1].text
 
     turoCsv.append(carInfo)
 
 def manageCSV(csvRows):
-    with open ('turo.csv', 'w',newline='') as file:
+    print('\n\n\n\n\n')
+    print('Making CSV')
+    with open ('turo '+datetime.today().strftime('%Y-%m-%d')+'.csv', 'w',newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(csvRows[0])
-        for x in range(1,len(csvRows)):
-            writer.writerow([csvRows[x]['guest'],csvRows[x]['reg'],csvRows[x]['status'],csvRows[x]['start'],csvRows[x]['end'],getFloat(csvRows[x]['miles']),0,getFloat(csvRows[x]['reimbursements']),getFloat(csvRows[x]['tripPrice']),getFloat(csvRows[x]['earnings'])])
+        temp=[]
+        count=0
+        indexKey={}
+        writer.writerow(temp)
+        for x in range(len(csvRows)):
+            temp=[''] * count
+            for y in csvRows[x]:
+                try:
+                    temp[indexKey[y]] = csvRows[x][y]
+                except KeyError:
+                    indexKey[y]=count
+                    temp.append('')
+                    temp[indexKey[y]] = csvRows[x][y]
+                    count+=1
+            try:
+                writer.writerow(temp)
+            except UnicodeEncodeError:
+                print(temp)
+        
+        csvHeader=[]
+        for x in indexKey:
+            csvHeader.append(x)
+        writer.writerow(csvHeader)
+        
+
 
 start=time.time()
 driver=setUpDriver()
 getEles('body')
 driver.get("https://turo.com/")
-print('\n\n\n\n\n\n\n\n\n')
 login()
-
-print('\n\n\n\n\n\n\n\n\n')
-getTripUpcomingAll()
+goToTrips('history')
+# getTripUpcomingAll()
 getTripHistoryAll()
-print('\n\n')
-
 
 manageCSV(turoCsv)
 print('Took',time.time()-start,'seconds')
-input('DONE')
+print('DONE')
